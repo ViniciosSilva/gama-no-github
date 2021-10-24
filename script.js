71248153b5bc516a8cc2b8c69c18b2e9281d6752
(Vinicios <(vi.harmonia@gmail.com)>
2021-10-23 21:57:35 -0300

    primeiro commit projeto formulario de cadastro

---
+++ script.js
@@ -0,0 +1,49 @@
+function validar(){
+  var nome = document.getElementById("nome");
+  var cpf = document.getElementById("cpf");
+  var endereco = document.getElementById("endereco");
+  var numero = document.getElementById("numero");
+  var cel= document.getElementById("cel");
+
+
+
+  if (nome.value == "") {
+    alert("Nome não informado");
+    nome.focus();
+return;
+}
+
+  if (cpf.value == "") {
+    alert("CPF não informado");
+    cpf.focus();
+return;
+}
+
+
+  if (endereco.value == "") {
+    alert("Endereço não informado");
+    endereco.focus();
+return;
+}
+
+  if (numero.value == "") {
+    alert("Número não informado");
+    numero.focus();
+return;
+}
+
+  if (cel.value == "") {
+    alert("Celular não informado");
+    cel.focus();
+return;
+}
+  alert("Formulário enviado!");
+
+  formulario.submit();
+}
+
+
+
+
+
+
